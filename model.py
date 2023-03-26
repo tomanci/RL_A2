4b2577df71f51e17a54724c67f9705b936ec10ac
Tom Stein <dev@tomstein.me>
2023-03-26 17:13:11 +0200

    fix cuda usage

--- model.py
+++ model.py
@@ -1,5 +1,4 @@
 import numpy as np
-import os
 import torch
 from nn import NeuralNetwork
 import torch.optim as optim
@@ -9,19 +8,19 @@ import torch.nn as nn
 class DQN:
 
     def __init__(self, alpha=0.0001):
-        device = "cuda" if torch.cuda.is_available() else "cpu"
-        self.model = NeuralNetwork().to(device)
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.model = NeuralNetwork().to(self.device)
         self.criterion = nn.MSELoss()
         self.optimizer = optim.Adam(self.model.parameters(), lr=alpha)
 
     def forward_pass_no_grad(self, s):
-        input_data = torch.tensor(np.copy(s))
+        input_data = torch.tensor(np.copy(s)).to(self.device)
         torch.reshape(input_data, (1, 4))
         with torch.no_grad():
-            return self.model(input_data)
+            return self.model(input_data).cpu()
 
     def forward_pass(self, x):
-        return self.model(x)
+        return self.model(x.to(self.device)).cpu()
 
     def compute_loss(self, output, target):
         # return (target - output).pow(2).sum()
